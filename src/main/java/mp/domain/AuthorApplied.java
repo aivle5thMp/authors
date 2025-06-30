42bf7076ecc923ca384ab75886c492989f4c07bb
upz <smilish67@naver.com>
2025-07-01 00:20:35 +0900

    initial commit

---
+++ src/main/java/mp/domain/AuthorApplied.java
@@ -0,0 +1,30 @@
+package mp.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import mp.domain.*;
+import mp.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class AuthorApplied extends AbstractEvent {
+
+    private UUID id;
+    private UUID userId;
+    private String info;
+    private String status;
+    private String portfolioUrl;
+    private String name;
+    private String bio;
+
+    public AuthorApplied(Author aggregate) {
+        super(aggregate);
+    }
+
+    public AuthorApplied() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
---
+++ src/main/java/mp/domain/EnrollView.java
@@ -0,0 +1,29 @@
+package mp.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+//<<< EDA / CQRS
+@Entity
+@Table(name = "EnrollView_table")
+@Data
+public class EnrollView {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID userId;
+    private String enrollStatus;
+    private String info;
+    private String portfolioUrl;
+    private String status;
+}
